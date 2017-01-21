7bb54189fb6edd22da5cac509f2cef1f4fce549a
raegu14 <raegu14@gmail.com>
2017-01-20 20:44:03 -0500

    Added Car Script

---
+++ Waves/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+    Vector3 movement;
+
+	// Use this for initialization
+	void Start () {
+        movement = new Vector3(1, 1, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.position + movement * Time.deltaTime;
+	}
+}
