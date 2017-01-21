711e80f190ee21e6acc88fb5e3a4840476f9b29b
raegu14 <raegu14@gmail.com>
2017-01-20 22:16:59 -0500

    added Intersection

--- Waves/Assets/Scripts/CarMovement.cs
+++ Waves/Assets/Scripts/CarMovement.cs
@@ -6,16 +6,20 @@ public class CarMovement : MonoBehaviour {
 
     Vector3 movement;
     GameObject cop;
+    public bool pass = false; // pass intersection
 
 	// Use this for initialization
 	void Start () {
         cop = GameObject.FindGameObjectWithTag("cop");
         movement = new Vector3(1, 1, 0);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-       if (cop.GetComponent<CopControls>().a)
-        transform.position = transform.position + movement * Time.deltaTime;
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (cop.GetComponent<CopControls>().a || pass)
+        {
+            transform.position = transform.position + movement * Time.deltaTime;
+        }
+    }
 }
