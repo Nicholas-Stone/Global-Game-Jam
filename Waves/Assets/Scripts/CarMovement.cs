7528c6dce75df3aa6d237f9d45551c63176f963e
raegu14 <raegu14@gmail.com>
2017-01-20 21:07:03 -0500

    Added direction toggle for cop and car

--- Waves/Assets/Scripts/CarMovement.cs
+++ Waves/Assets/Scripts/CarMovement.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class CarMovement : MonoBehaviour {
 
     Vector3 movement;
+    GameObject cop;
 
 	// Use this for initialization
 	void Start () {
+        cop = GameObject.FindGameObjectWithTag("cop");
         movement = new Vector3(1, 1, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+       if (cop.GetComponent<CopControls>().a)
         transform.position = transform.position + movement * Time.deltaTime;
 	}
 }
---
+++ Waves/Assets/Scripts/CopControls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopControls : MonoBehaviour {
+
+    public bool w, a, s, d;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown("w"))
+        {
+            w = true;
+            print("w");
+        }
+        if (Input.GetKeyDown("a"))
+        {
+            a = !a;
+            print("a");
+        }
+        if (Input.GetKeyDown("s"))
+        {
+            s = true;
+            print("s");
+        }
+        if (Input.GetKeyDown("d"))
+        {
+            d = true;
+            print("d");
+        }
+    }
+}
